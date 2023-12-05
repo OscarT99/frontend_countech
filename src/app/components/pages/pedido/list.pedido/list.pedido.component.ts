d18a87173cd7e511fbde2cec29f5402e731cf689
OscarT99 <oatorres004@misena.edu.co>
2023-12-04 23:14:37 -0500

    detalle de pedido

--- src/app/components/pages/pedido/list.pedido/list.pedido.component.ts
+++ src/app/components/pages/pedido/list.pedido/list.pedido.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Table } from 'primeng/table';
 import { PedidoService } from 'src/app/services/pedido/pedido.service'; 
 import { PedidoInstance } from 'src/app/interfaces/pedido/pedido.interface'; 
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Dialog } from 'primeng/dialog';
 
 
 @Component({
@@ -14,6 +15,11 @@ export class ListPedidoComponent implements OnInit {
     listPedidos: PedidoInstance[] = []
     pedido: PedidoInstance = {}
     id:number=0;
+
+    mostrarModalDetalle: boolean = false;
+    pedidoIdSeleccionado!: number;
+    detallePedido: any; // Puedes ajustar esto según la estructura de tu pedido
+    @ViewChild('detallePedidoModal') detallePedidoModal!: Dialog;
    
     rowsPerPageOptions = [5, 10, 15];
 
@@ -44,18 +50,33 @@ export class ListPedidoComponent implements OnInit {
         this.router.navigate(['/pages/pedido/add', id]);
       }
       
-eliminarPedido(id: number, estado: string): void {
-  if (estado === 'Registrado') {
-      this._pedidoService.deletePedido(id).subscribe(() => {
-          this.toastr.success('Pedido eliminado correctamente', 'Éxito');
-          this.getListPedidos();
-      }, (error) => {
-          this.toastr.error('Error al eliminar el pedido', 'Error');
-          console.error(error);
-      });
-  } else {
-      this.toastr.warning('No se puede eliminar un pedido con estado diferente a "Registrado"', 'Advertencia');
-  }
-}
+    eliminarPedido(id: number, estado: string): void {
+    if (estado === 'Registrado') {
+        this._pedidoService.deletePedido(id).subscribe(() => {
+            this.toastr.success('Pedido eliminado correctamente', 'Éxito');
+            this.getListPedidos();
+        }, (error) => {
+            this.toastr.error('Error al eliminar el pedido', 'Error');
+            console.error(error);
+        });
+    } else {
+        this.toastr.warning('No se puede eliminar un pedido con estado diferente a "Registrado"', 'Advertencia');
+    }
+    }
+
+    async mostrarDetallePedido(id: number) {
+        try {
+            this.detallePedido = await this._pedidoService.getPedido(id).toPromise();
+            console.log('Detalle del pedido:', this.detallePedido);
+            this.mostrarModalDetalle = true;
+        } catch (error) {
+            console.error('Error al obtener el detalle del pedido:', error);
+        }
+    }
+    
+      cerrarModalDetallePedido() {
+        this.mostrarModalDetalle = false;
+        this.detallePedido = null; // Limpia los detalles al cerrar el modal
+      }
 
 }
